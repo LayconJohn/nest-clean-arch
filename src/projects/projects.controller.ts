462dc3f5322e25189ee26a28295fa85ced50befd
Laycon John <laycon.gomes@tld.local>
2024-01-23 11:39:49 -0300

    refactor: use case create project

--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -2,14 +2,18 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { ProjectsService } from './projects.service';
 import { CreateProjectDto } from './dto/create-project.dto';
 import { UpdateProjectDto } from './dto/update-project.dto';
+import { CreateProjectUseCase } from './use-cases/create-project.use-case';
 
 @Controller('projects')
 export class ProjectsController {
-  constructor(private readonly projectsService: ProjectsService) {}
+  constructor(
+    private readonly projectsService: ProjectsService,
+    private readonly createProjectUseCase: CreateProjectUseCase
+    ) {}
 
   @Post()
   create(@Body() createProjectDto: CreateProjectDto) {
-    return this.projectsService.create(createProjectDto);
+    return this.createProjectUseCase.execute(createProjectDto);
   }
 
   @Get()
--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -3,13 +3,14 @@ import { ProjectsService } from './projects.service';
 import { ProjectsController } from './projects.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Project } from './entities/project.entity';
+import { CreateProjectUseCase } from './use-cases/create-project.use-case';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Project])
   ],
   controllers: [ProjectsController],
-  providers: [ProjectsService],
+  providers: [ProjectsService, CreateProjectUseCase],
 })
 export class ProjectsModule {}
  
